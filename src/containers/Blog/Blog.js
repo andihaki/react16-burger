02f9477803267065f68ce910f76e7bb1b88aa549
Andi Hakim <andi.hakim.arif@gmail.com>
2018-06-11 20:59:50 +0700

    react-router, react-router-dom

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -48,16 +48,18 @@ class Blog extends Component {
             });
         }
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                </section>                
             </div>
         );
     }
