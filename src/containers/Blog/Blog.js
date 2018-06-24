5a19d27f44d6e1685d567371cabba64768f0794d
Andi Hakim <andi.hakim.arif@gmail.com>
2018-06-24 16:45:53 +0700

    lazy load page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom'; // NavLink rada beda dengan Link, soalnya otomatis nambah class="active"
+import { Route, NavLink, Switch } from 'react-router-dom'; // NavLink rada beda dengan Link, soalnya otomatis nambah class="active"
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
-class Blog extends Component {    
+const AsyncNewPost = asyncComponent(_ => { // _ === ()
+    return import('./NewPost/NewPost');
+});
+
+class Blog extends Component {   
+    state = {
+        auth: true,
+    } 
     render () {        
         return (
             <div className="Blog">
@@ -32,9 +40,10 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <Posts />}/>
                 <Route path="/new-post" render={() => <h1>newpost</h1>}/> */}
                 <Switch> {/*supaya stop lookup pas udah ketemu path new-post*/}
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>404 Not Found</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
                     {/* <Route path="/posts" component={Posts} /> */}
                 </Switch>
             </div>
--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -25,7 +25,8 @@ class NewPost extends Component {
         axios.post('/posts/', data)
             .then(response => {
                 console.log(response);
-                this.setState({ submitted: true });
+                this.props.history.replace('/posts'); // .replace = back button not working, .push = back button worked
+                // this.setState({ submitted: true }); 
             })
     }
 
