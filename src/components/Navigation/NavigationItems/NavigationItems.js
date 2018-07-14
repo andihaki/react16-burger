a9167583b371b433108ee9a9142a9f4a04959094
Andi Hakim <andi.hakim.arif@gmail.com>
2018-07-14 17:55:58 +0700

    rename navigation bar

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,7 @@ const navigationItems = (props) => (
     <ul className={css.NavigationItems}>
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders" >Orders</NavigationItem>
-        <NavigationItem link="/auth" >Auth</NavigationItem>
+        <NavigationItem link="/auth" >Authenticate</NavigationItem>
     </ul>
 );
 
